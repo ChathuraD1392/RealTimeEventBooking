53182a8e3752045eb1fd9cf42383dda90f2507e7
ChathuraD1392 <chathuradilshan1392@gmail.com>
2024-12-10 22:16:27 +0530

    initial commit

---
+++ client/src/App.tsx
@@ -0,0 +1,14 @@
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <div>
+        <h1>Hello World</h1>
+        <button className="btn btn-primary">Click Me</button>
+      </div>
+    </>
+  );
+}
+
+export default App;
